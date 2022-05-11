bc5f2988a4fbe6105dfc646d6702310bd6fb0102
박하민 <haminjp@naver.com>
2022-05-11 20:46:47 +0900

    [수정] ResourceListItem 컴포넌트 수정

--- src/components/ResourceListItem.tsx
+++ src/components/ResourceListItem.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import styled from "styled-components";
 import EditIcon from "../img/editIcon.png";
 import TrashIcon from "../img/trashIcon.png";
@@ -20,16 +21,50 @@ const ButtonContainer = styled.div`
   display: flex;
   justify-content: right;
   gap: 8px;
+  cursor: pointer;
+`;
+
+const TextRead = styled.span`
+  font-size: 14px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+const TextWrite = styled.input`
+  width: 100%;
+  height: 30px;
+  font-size: 14px;
+  border: 1px solid #38a5e1;
+  border-radius: 5px;
+  background-color: #f7f7f7;
 `;
 
 const ResourceListItem = () => {
+  const [value, setValue] = useState<string>("");
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+
+  const onEditClick = () => {
+    setIsEdit((prevIsEdit) => !prevIsEdit);
+  };
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newInputValue = e.target.value;
+    setValue(newInputValue);
+  };
+
   return (
     <Container>
       <InputContainer>
-        <input type="text" />
+        {isEdit && (
+          <TextWrite type="text" value={value} onChange={onInputChange} />
+        )}
+        {!isEdit && <TextRead>{value}</TextRead>}
       </InputContainer>
       <ButtonContainer>
-        <img src={EditIcon} alt="edit" />
+        <img src={EditIcon} alt="edit" onClick={onEditClick} />
         <img src={TrashIcon} alt="delete" />
       </ButtonContainer>
     </Container>
