e04a3d8018dc12b005e7d379767748df76338bb0
박하민 <haminjp@naver.com>
2022-05-11 19:47:13 +0900

    [추가] 리소스 아이템 뷰 컴포넌트 구현

--- src/components/ResourceList.tsx
+++ src/components/ResourceList.tsx
@@ -1,13 +1,58 @@
 import styled from "styled-components";
+import ResourceListItem from "./ResourceListItem";
 
 const Container = styled.div`
-  background-color: yellow;
   width: 280px;
   border-right: 1px solid #c4c4c4;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  height: 50px;
+  border-bottom: 1px solid #c4c4c4;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  gap: 10px;
+`;
+
+const AddButton = styled.button`
+  width: 125px;
+  height: 30px;
+  border: 1px solid #e5e5e5;
+  border-radius: 5px;
+  background-color: white;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 0;
+`;
+
+const List = styled.div`
+  background-color: #f7f7f7;
+  width: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px 0;
+  gap: 10px;
 `;
 
 const ResourceList = () => {
-  return <Container>ResourceList</Container>;
+  return (
+    <Container>
+      <Header>
+        <AddButton>URL 추가</AddButton>
+        <AddButton>이미지 추가</AddButton>
+      </Header>
+      <List>
+        <ResourceListItem />
+        <ResourceListItem />
+        <ResourceListItem />
+      </List>
+    </Container>
+  );
 };
 
 export default ResourceList;
---
+++ src/components/ResourceListItem.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import EditIcon from "../img/editIcon.png";
+import TrashIcon from "../img/trashIcon.png";
+
+const Container = styled.div`
+  background-color: white;
+  width: 260px;
+  height: 90px;
+  border-radius: 10px;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const InputContainer = styled.div`
+  flex: 1;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: right;
+`;
+
+const ResourceListItem = () => {
+  return (
+    <Container>
+      <InputContainer>
+        <input type="text" />
+      </InputContainer>
+      <ButtonContainer>
+        <img src={EditIcon} alt="edit" />
+        <img src={TrashIcon} alt="delete" />
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+export default ResourceListItem;
