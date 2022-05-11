6acd147fca189825742d2bac7569d8990a8faf58
박하민 <haminjp@naver.com>
2022-05-11 19:24:58 +0900

    [추가] 기본 App 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+import ResourceList from "./components/ResourceList";
+import Viewer from "./components/Viewer";
+
+const Page = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+`;
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Page>
+      <ResourceList />
+      <Viewer />
+    </Page>
+  );
 };
 
 export default App;
---
+++ src/components/ResourceList.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: yellow;
+  width: 280px;
+  border-right: 1px solid #c4c4c4;
+`;
+
+const ResourceList = () => {
+  return <Container>ResourceList</Container>;
+};
+
+export default ResourceList;
